a8722709480a22827704326b5a0f4699e6e1cea0
Jérémy Marcillaud <jeremy@syncly.fr>
2023-01-26 13:57:12 +0100

    add Date to response of getPreviousMonth

--- src/utils.ts
+++ src/utils.ts
@@ -16,7 +16,7 @@ export function totalPerCategory(
   }, {});
 }
 
-export function getPreviousMonth(d: Date) {
+export function getPreviousMonth(d: Date): Date {
   d.setDate(1);
   d.setMonth(d.getMonth() - 1);
   return d;
